d7b54443bd906cac9003f46a1c996428a80a9463
davydocsurg <davydocsurg@gmail.com>
2023-09-21 15:15:50 +0100

    chore: add comments and replies table #1

---
+++ database/migrations/2023_09_21_140506_create_comments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->integer('comment_id')->unique();
+            $table->text('text');
+            $table->dateTime('time', $precision = 0);
+            $table->string('type');
+            $table->foreignId('story_id')->constrained('stories')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('author_id')->constrained('authors')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('parent_comment_id')->constrained('comments')->onUpdate('cascade')->onDelete('cascade')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comments');
+    }
+};
