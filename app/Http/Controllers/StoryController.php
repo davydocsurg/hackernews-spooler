8b84679b1d3f2858c460acf2f17f12f7f1c1995c
davydocsurg <davydocsurg@gmail.com>
2023-09-23 10:48:44 +0100

    chore: add StoryController #3

---
+++ app/Http/Controllers/StoryController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Jobs\FetchStoriesJob;
+use App\Services\HackernewsService;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
+class StoryController extends Controller
+{
+    /**
+     * Dispatch the job to fetch stories.
+     *
+     * @param Request $request
+     * @return Response
+     */
+    public function fetchAndStoreStories(HackernewsService $hackernewsService)
+    {
+        try {
+            $job = new FetchStoriesJob($hackernewsService);
+            $this->dispatch($job);
+
+            return response()->json([
+                'message' => 'Fetching stories job dispatched.',
+            ]);
+        } catch (\Throwable $e) {
+            // Handle any exceptions or errors here
+            Log::error('Error dispatching FetchStoriesJob: ' . $e->getMessage());
+
+            return response()->json([
+                'error' => 'An error occurred while dispatching the job.',
+            ], 500); // Return a 500 Internal Server Error status code
+        }
+    }
+}
